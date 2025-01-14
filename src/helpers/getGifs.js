022229a70c8d048230435fc104eb0185b48daef2
nikogalvan <ramiro_coronel333@hotmail.com>
2025-01-14 13:01:25 -0300

    add config vite & gitignore

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,5 @@
 export const getGifs = async (category) => {
-  const API_KEY = "iPNO8CU8xtRxpVct7ejWxaO1RaC9tk3y";
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=5`;
-  
+  const url = `http://api.giphy.com/v1/gifs/search?api_key=${import.meta.env.VITE_REACT_APP_GIPHY_API}&q=${category}&limit=5`;
   const response = await fetch(url);
   const { data } = await response.json();
   const gifs = data.map((img) => ({
