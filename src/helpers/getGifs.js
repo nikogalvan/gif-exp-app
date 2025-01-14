22654c41a5b617e02bc2a50a7a323364358cd002
nikogalvan <ramiro_coronel333@hotmail.com>
2025-01-14 13:33:25 -0300

    modify url

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,7 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=${import.meta.env.VITE_REACT_APP_GIPHY_API}&q=${category}&limit=5`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${
+    import.meta.env.VITE_REACT_APP_GIPHY_API
+  }&q=${category}&limit=5`;
   const response = await fetch(url);
   const { data } = await response.json();
   const gifs = data.map((img) => ({
@@ -7,6 +9,6 @@ export const getGifs = async (category) => {
     title: img.title,
     url: img.images.downsized_medium.url,
   }));
-  
+
   return gifs;
 };
